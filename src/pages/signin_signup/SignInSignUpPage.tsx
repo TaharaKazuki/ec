70e6fdb2c9e230d5279dca1e7c1c06e3bc812d66
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-28 20:55:59 +0900

    update

--- src/pages/signin_signup/SignInSignUpPage.tsx
+++ src/pages/signin_signup/SignInSignUpPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import SignIn from '../../components/signIn/SignIn'
+
+import './style/signInSignUpPage.scss'
 
 const SignInSignUpPage = () => {
-  return <div></div>
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
 }
 
 export default SignInSignUpPage
