efffa080d1503b77d6541b73763c1ba5f1a71df1
Tahara Kazuki <>
2021-12-03 18:46:18 +0900

    [update]

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, FC } from 'react'
 //router
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 // page component
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage'
@@ -53,7 +53,13 @@ const App: FC = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInSignUpPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInSignUpPage />
+          }
+        />
       </Switch>
     </div>
   )
