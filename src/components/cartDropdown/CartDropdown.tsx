272991dcad57c1854407d3ba96591134071fd949
Tahara Kazuki <>
2021-12-05 20:23:53 +0900

    update

---
+++ src/components/cartDropdown/CartDropdown.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CustomButton from '../customButton/CustomButton'
+
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items" />
+      <CustomButton />
+    </div>
+  )
+}
+
+export default CartDropdown
